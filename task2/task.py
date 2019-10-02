1309ead4ec6612d2b8000ed0e93a5acf5f6ce6b7
Nasrin Tohidi <nasrin@kidocode.com>
2019-10-02 15:59:06 +0800

    Update task.py
    change inline if to if

--- task2/task.py
+++ task2/task.py
@@ -1,4 +1,7 @@
 #Create a function get a number and returns true if its even anf alse if its odd
 def checkMe(n):
-    return n % 2 == 0
+    if n % 2 == 0:
+        return True
+    else:
+        return False
     pass
